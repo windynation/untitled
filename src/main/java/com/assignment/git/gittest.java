626574f28a7afbf07b76d939fc5dcc9fafc0fc9b
Your Name <you@example.com>
2022-10-04 19:31:15 +0800

    hot-fix commit 1

--- src/main/java/com/assignment/git/gittest.java
+++ src/main/java/com/assignment/git/gittest.java
@@ -3,5 +3,6 @@ package com.assignment.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("Helloworld");
+        System.out.println("Helloworld2");
     }
 }
