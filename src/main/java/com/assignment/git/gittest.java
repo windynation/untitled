dc2a7ef2400ad7aa1e7c6270aa3cf7b8bbacfbd6
Your Name <you@example.com>
2022-10-04 19:52:06 +0800

    master commit 3

--- src/main/java/com/assignment/git/gittest.java
+++ src/main/java/com/assignment/git/gittest.java
@@ -5,6 +5,6 @@ public class gittest {
         System.out.println("Helloworld");
         System.out.println("Helloworld2");
         System.out.println("Helloworld2");
-        System.out.println("Helloworld3");
+        System.out.println("Helloworld4");
     }
 }
